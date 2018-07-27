a18b74e506395a585d0f15d5789f63150223f4a9
Diego Camacho <hackvan@gmail.com>
2018-07-27 14:49:44 -0500

    Make SQL files

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id          Integer Primary Key,
+  name        Text,
+  age         Integer,
+  gender      Char(1),
+  color       Text,
+  temperament Text,
+  alive       Boolean
+);
